a44403d1583c82fdcc395532eab3a54cbe479905
Borrdom <86350432+Borrdom@users.noreply.github.com>
2023-07-21 19:44:19 +0200

    cleaned up the PyQT GUI and added rotate and xlimit buttons

--- ExcelSheet/csvplot.py
+++ ExcelSheet/csvplot.py
@@ -7,6 +7,7 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg,NavigationToolb
 import os
 from PyQt5 import QtCore, QtWidgets
 import sys
+from matplotlib.ticker import AutoLocator
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -22,39 +23,93 @@ class MainWindow(QtWidgets.QMainWindow):
         # Create buttons
         self.button_x_limits = QtWidgets.QPushButton("Adjust X Limits")
         self.button_y_limits = QtWidgets.QPushButton("Adjust Y Limits")
-        self.button_x_ticks = QtWidgets.QPushButton("Adjust X Ticks")
-        self.button_y_ticks = QtWidgets.QPushButton("Adjust Y Ticks")
+        self.button_reset_ticks = QtWidgets.QPushButton("Reset Ticks")
+        self.button_rotate_vertical = QtWidgets.QPushButton("Rotate y-Axis vertical")
+        self.button_rotate_horizontal = QtWidgets.QPushButton("Rotate y-Axis horizontal")
 
         # Create entry boxes
-        self.entry_x_limits = QtWidgets.QLineEdit()
-        self.entry_y_limits = QtWidgets.QLineEdit()
-        self.entry_x_ticks = QtWidgets.QLineEdit()
-        self.entry_y_ticks = QtWidgets.QLineEdit()
+        start, end = ax.get_xlim()
+        self.entry_x_min = QtWidgets.QLineEdit(str(start))
+        self.entry_x_max = QtWidgets.QLineEdit(str(end))
+        self.entry_x_last = QtWidgets.QLineEdit(str(end))
+        self.entry_x_ticks = QtWidgets.QLineEdit("4")
+        start, end = ax.get_ylim()
+        self.entry_y_min = QtWidgets.QLineEdit(str(start))
+        self.entry_y_max = QtWidgets.QLineEdit(str(end))
+        self.entry_y_last = QtWidgets.QLineEdit(str(end))
+        self.entry_y_ticks = QtWidgets.QLineEdit("4")
+        #Create x limit info
+        
+        self.xmin_label=QtWidgets.QLabel("xmin")
+        self.xmax_label=QtWidgets.QLabel("xmax")
+        self.xlast_label=QtWidgets.QLabel("xend")
+        self.nxticks_label=QtWidgets.QLabel("nxticks")
+        #Create y limit info
+        self.ymin_label=QtWidgets.QLabel("ymin")
+        self.ymax_label=QtWidgets.QLabel("ymax")
+        self.ylast_label=QtWidgets.QLabel("yend")
+        self.nyticks_label=QtWidgets.QLabel("nyticks")
+        
 
         # Connect button signals to respective slots
         self.button_x_limits.clicked.connect(self.adjust_x_limits)
         self.button_y_limits.clicked.connect(self.adjust_y_limits)
-        self.button_x_ticks.clicked.connect(self.adjust_x_ticks)
-        self.button_y_ticks.clicked.connect(self.adjust_y_ticks)
+        self.button_reset_ticks.clicked.connect(self.reset_ticks)
+        self.button_rotate_vertical.clicked.connect(self.vertical_ylabel)
+        self.button_rotate_horizontal.clicked.connect(self.horizontal_ylabel)
+
+
+
+        #Create Button Layout
+        button_layout = QtWidgets.QVBoxLayout()
+
+        #adjust xlim labels 
+        xlim_label_layout = QtWidgets.QHBoxLayout()
+        xlim_label_layout.addWidget(self.xmin_label)
+        xlim_label_layout.addWidget(self.xmax_label)
+        xlim_label_layout.addWidget(self.xlast_label)
+        xlim_label_layout.addWidget(self.nxticks_label)
+        button_layout.addLayout(xlim_label_layout)
+        #adjust xlim entries
+        xlim_layout = QtWidgets.QHBoxLayout()
+        xlim_layout.addWidget(self.entry_x_min)
+        xlim_layout.addWidget(self.entry_x_max)
+        xlim_layout.addWidget(self.entry_x_last)
+        xlim_layout.addWidget(self.entry_x_ticks)
+        button_layout.addLayout(xlim_layout)
+        #adjust xlim button
+        button_layout.addWidget(self.button_x_limits)
+        
+        #adjust ylim labels 
+        ylim_label_layout = QtWidgets.QHBoxLayout()
+        ylim_label_layout.addWidget(self.ymin_label)
+        ylim_label_layout.addWidget(self.ymax_label)
+        ylim_label_layout.addWidget(self.ylast_label)
+        ylim_label_layout.addWidget(self.nyticks_label)
+        button_layout.addLayout(ylim_label_layout)
+        #adjust ylim entries
+        ylim_layout = QtWidgets.QHBoxLayout()
+        ylim_layout.addWidget(self.entry_y_min)
+        ylim_layout.addWidget(self.entry_y_max)
+        ylim_layout.addWidget(self.entry_y_last)
+        ylim_layout.addWidget(self.entry_y_ticks)
+        button_layout.addLayout(ylim_layout)
+        #adjust ylim button
+        button_layout.addWidget(self.button_y_limits)
+        #add a reset button
+        button_layout.addWidget(self.button_reset_ticks)
+
+        # add rotation
+        button_layout.addWidget(self.button_rotate_horizontal)
+        button_layout.addWidget(self.button_rotate_vertical)
+
+        #Stretch until the end of the layout
+        button_layout.addStretch(1)
 
         # Create the toolbar and add it to the layout
+        plot_layout = QtWidgets.QHBoxLayout()
         toolbar = NavigationToolbar2QT(sc, self,coordinates=False)
         toolbar.setOrientation(QtCore.Qt.Vertical)
-        button_layout = QtWidgets.QVBoxLayout()
-        
-        
-
-        button_layout.addWidget(self.button_x_limits)
-        button_layout.addWidget(self.entry_x_limits)
-        button_layout.addWidget(self.button_y_limits)
-        button_layout.addWidget(self.entry_y_limits)
-        button_layout.addWidget(self.button_x_ticks)
-        button_layout.addWidget(self.entry_x_ticks)
-        button_layout.addWidget(self.button_y_ticks)
-        button_layout.addWidget(self.entry_y_ticks)
-        
-        plot_layout = QtWidgets.QHBoxLayout()
-
         plot_layout.addWidget(toolbar)
         plot_layout.addWidget(sc)
         
@@ -68,47 +123,46 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(widget)
         self.show()
 
+    def reset_ticks(self):
+        self.ax.xaxis.set_major_locator(AutoLocator())
+        self.ax.yaxis.set_major_locator(AutoLocator())
+        self.canvas.draw()
+
+    def vertical_ylabel(self):
+        self.ax.yaxis.label.set(rotation=90)
+        self.ax.yaxis.set_label_coords(-0.15,0.5)
+        self.canvas.draw()
+    def horizontal_ylabel(self):
+        self.ax.yaxis.label.set(rotation=0)
+        self.ax.yaxis.set_label_coords(-0.1,1)
+        self.canvas.draw()
+
     def adjust_x_limits(self):
         # Get the x limits from the entry box
-        x_limits = self.entry_x_limits.text()
+        x_min, x_max = self.entry_x_min.text(),self.entry_x_max.text()
+        x_last= self.entry_x_last.text()
+        x_ticks = self.entry_x_ticks.text()
         try:
-            x_min, x_max = map(float, x_limits.split(','))
-            # Code for adjusting x limits
-            self.ax.set_xlim(x_min, x_max)
+            self.ax.set_xlim(float(x_min), float(x_max))
+            self.ax.xaxis.set_ticks(np.linspace(float(x_min), float(x_last), int(x_ticks)))
+            # self.ax.xaxis.set_major_locator(AutoLocator())
             self.canvas.draw()
         except ValueError:
             QtWidgets.QMessageBox.warning(self, "Invalid Input", "Please enter valid x limits.")
 
     def adjust_y_limits(self):
         # Get the y limits from the entry box
-        y_limits = self.entry_y_limits.text()
+        y_min, y_max = self.entry_y_min.text(),self.entry_y_max.text()
+        y_last= self.entry_y_last.text()
+        y_ticks = self.entry_y_ticks.text()
         try:
-            y_min, y_max = map(float, y_limits.split(','))
-            # Code for adjusting y limits
-            self.ax.set_ylim(y_min, y_max)
+            self.ax.set_ylim(float(y_min), float(y_max))
+            self.ax.yaxis.set_ticks(np.linspace(float(y_min), float(y_last) , int(y_ticks)))
+            # self.ax.yaxis.set_major_locator(AutoLocator())
             self.canvas.draw()
         except ValueError:
             QtWidgets.QMessageBox.warning(self, "Invalid Input", "Please enter valid y limits.")
 
-    def adjust_x_ticks(self):
-        # Get the x ticks from the entry box
-        x_ticks = self.entry_x_ticks.text()
-        start, end = self.ax.get_xlim()
-        try:
-            self.ax.xaxis.set_ticks(np.linspace(start, end, int(x_ticks)))
-            self.canvas.draw()
-        except ValueError:
-            QtWidgets.QMessageBox.warning(self, "Invalid Input", "Please enter valid x ticks.")
-
-    def adjust_y_ticks(self):
-        # Get the y ticks from the entry box
-        y_ticks = self.entry_y_ticks.text()
-        start, end = self.ax.get_ylim()
-        try:
-            self.ax.yaxis.set_ticks(np.linspace(start, end, int(y_ticks)))
-            self.canvas.draw()
-        except ValueError:
-            QtWidgets.QMessageBox.warning(self, "Invalid Input", "Please enter valid y ticks.")
 
 
 
@@ -139,7 +193,9 @@ class origin_like:
         ax.set_ylabel(f'$\mathrm{{{ylabel1}}}$ \n / $\mathrm{{{yunit1}}}$',rotation=0,loc="top",linespacing=1.5) if yunit1 is not None else ax.set_ylabel(f'$\mathrm{{{ylabel1}}}$',linespacing=1.5)
     def plot(ax,x,y,yerr,Formatstring,label=None,order=1):
         if sum(yerr)==0 : yerr=None
-        ax.errorbar(x,y,yerr,None,Formatstring , zorder=order,linewidth = 1.5,label=label,markersize=5, markeredgecolor='k',markeredgewidth=0.5,capsize=5, elinewidth=0.5,ecolor="k")
+        ax.plot(x,y,Formatstring , zorder=order,linewidth = 1.5,label=label,markersize=5, markeredgecolor='k',markeredgewidth=0.5)
+        ax.errorbar(x,y,yerr,None,"ko" , zorder=order-1,label=label,markersize=0, markeredgecolor='k',markeredgewidth=0.5,capsize=5, elinewidth=0.5,ecolor="k")
+        # ax.plot(x,y,Formatstring , zorder=order,linewidth = 1.5,label=label,markersize=5, markeredgecolor='k',markeredgewidth=0.5)
     def set_ticks(ax,x0=None,x1=None,y0=None,y1=None):
         if x0 is not None: ax.axis([x0, x1, y0, y1]) 
         start, end = ax.get_xlim()
@@ -170,7 +226,9 @@ while True:
 
 
 app = QtWidgets.QApplication(sys.argv)
+
 def Plot(df,title):
+    k=0
     fig, ax = origin_like.subplots()
     xlabel=df.values[0,0]
     ylabel=df.values[0,1]
@@ -184,8 +242,9 @@ def Plot(df,title):
             Formatstring=df.values[3,i]
             y=df.values[4:,i]
         if i%3==2:
+            k+=1
             yerr=df.values[4:,i]
-            origin_like.plot(ax,x.astype(float),y.astype(float),yerr.astype(float), Formatstring, label=legend)            
+            origin_like.plot(ax,x.astype(float),y.astype(float),yerr.astype(float), Formatstring, label=legend,order=k)            
     canvas=FigureCanvasQTAgg(fig)
     canvas.setFixedSize(1000,1000)
     canvas.draw()
